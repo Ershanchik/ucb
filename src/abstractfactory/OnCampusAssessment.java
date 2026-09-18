124a5d4893c4426fd99b6712469eeaf23ff5129c
Ershanchik <aru01kzz74@gmail.com>
2026-09-18 12:32:10 +0500

    sruz

--- src/abstractfactory/OnCampusAssessment.java
+++ src/abstractfactory/OnCampusAssessment.java
@@ -1,14 +1,7 @@
 package abstractfactory;
-
-/** On-campus variant of product #2: paper exam in a room. */
-public class OnCampusAssessment implements Assessment {
+public class OnCampusAssessment implements Assessment{
     @Override
-    public String rules() {
-        return "Written exam in the exam hall, 2 hours, closed book, invigilated";
-    }
-
+    public String rules(){return "Written exam in the exam hall, 2 hours, closed book, invigilated";}
     @Override
-    public int passMark() {
-        return 50;
-    }
+    public int passMark(){return 50;}
 }
